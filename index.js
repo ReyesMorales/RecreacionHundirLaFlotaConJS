c65595af069530f688e339fff6245cf0ac675e45
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-01-22 20:09:53 +0000

    Initialize players

---
+++ index.js
@@ -0,0 +1,13 @@
+const { getEmptyBoard } = require("./utils");
+
+const player1 = {
+    counter: 0,
+    ownBoard: getEmptyBoard(),
+    rivalBoard: getEmptyBoard(), 
+}
+
+const player2 = {
+    counter: 0,
+    ownBoard: getEmptyBoard(),
+    rivalBoard: getEmptyBoard(), 
+}
