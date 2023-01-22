b692782c0796e0d5904796e4370dc3891d3cdf67
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-01-22 19:44:00 +0000

    Add constants

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+const EMPTY_SPACE = '-';
+const BOAT_SPACE = 'x';
+const WATER_SPACE = 'W';
+const HIT_SPACE = 'T';
+
+exports.BOAT_SPACE = BOAT_SPACE;
+exports.WATER_SPACE = WATER_SPACE;
+exports.EMPTY_SPACE = EMPTY_SPACE;
+exports.HIT_SPACE = HIT_SPACE;
