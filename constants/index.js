c922617b183c3088fb3e0c691c691c793cf3e104
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-01-22 21:15:26 +0000

    feat(): add emojis

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,11 @@
-const EMPTY_SPACE = '-';
-const BOAT_SPACE = 'x';
-const WATER_SPACE = 'W';
-const HIT_SPACE = 'T';
+const EMPTY_SPACE = ' ';
+const BOAT_SPACE = '🚢';
+const WATER_SPACE = '🌊';
+const HIT_SPACE = '💥';
+const MAX_SHOOTS = 100;
 
 exports.BOAT_SPACE = BOAT_SPACE;
 exports.WATER_SPACE = WATER_SPACE;
 exports.EMPTY_SPACE = EMPTY_SPACE;
 exports.HIT_SPACE = HIT_SPACE;
+exports.MAX_SHOOTS = MAX_SHOOTS;
