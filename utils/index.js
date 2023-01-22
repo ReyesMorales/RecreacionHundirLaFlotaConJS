24f324bda9855a3eace21d40718727b6597a0534
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-01-22 19:51:26 +0000

    Add function shoot

--- utils/index.js
+++ utils/index.js
@@ -14,7 +14,24 @@ function showBoard(board, player) {
     console.table(board);
 }
 
-
+function shoot(targetBoard, trackBoard, playerShoot) {
+    let x = getRandomNumber(9);
+    let y = getRandomNumber(9);
+    playerShoot.counter++;;
+    while(trackBoard[x][y] !== EMPTY_SPACE) {
+        x = getRandomNumber(9);
+        y = getRandomNumber(9);
+    }
+   
+    if (targetBoard[x][y] === BOAT_SPACE) {
+        trackBoard[x][y] = HIT_SPACE;
+        return "Hit";
+    } else {
+        trackBoard[x][y] = WATER_SPACE;
+        return "Water";
+    }
+}
 
 exports.getEmptyBoard = getEmptyBoard;
 exports.showBoard = showBoard;
+exports.shoot = shoot;
