6bc2c155975b4c4cee9ee0e6b91bd14ec7e2104d
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-06-20 22:16:51 +0100

    Fix victory function and  show boards at the end

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ addAllBoats(player2.ownBoard);
   let gameFinished = false;
   let winner = null;
 
-  while (gameFinished === false) {
+  while (!gameFinished) {
     await new Promise((resolve) => {
       setTimeout(() => {
         console.clear();
@@ -80,38 +80,45 @@ addAllBoats(player2.ownBoard);
         showBoard(player1.ownBoard);
         console.log("Rival board");
         showBoard(player1.rivalBoard);
-        if (player1.shootsCounter === 0) {
+        if (player1.shootsCounter === 0 || isVictory(player2.ownBoard)) {
           gameFinished = true;
-        }
-        if (isVictory(player1.ownBoard)) {
-          gameFinished = true;
-          console.log("PLAYER 1 WINS");
+          winner = player1;
         }
         resolve();
       }, 1000);
     });
+
+    if (!gameFinished) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          console.clear();
+          console.log("PLAYER 2 TURNS");
+          shoot(player1.ownBoard, player2.rivalBoard, player2);
+          console.log("Own board");
+          showBoard(player2.ownBoard);
+          console.log("Rival board");
+          showBoard(player2.rivalBoard);
+          if (player2.shootsCounter === 0 || isVictory(player1.ownBoard)) {
+            gameFinished = true;
+            winner = player2;
+          }
+          resolve();
+        }, 1000);
+      });
+    }
+  }
+
+  if (winner) {
     await new Promise((resolve) => {
       setTimeout(() => {
         console.clear();
-        console.log("PLAYER 2 TURNS");
-        shoot(player1.ownBoard, player2.rivalBoard, player2);
-        console.log("Own board");
+        console.log(`Congratulations! ${winner.id} wins the game!`);
+        console.log("Player 1 Board:");
+        showBoard(player1.ownBoard);
+        console.log("Player 2 Board:");
         showBoard(player2.ownBoard);
-        console.log("Rival board");
-        showBoard(player2.rivalBoard);
-        if (player2.shootsCounter === 0) {
-          gameFinished = true;
-        }
-        if (isVictory(player2.ownBoard)) {
-          gameFinished = true;
-          console.log("PLAYER 2 WINS");
-        }
         resolve();
-      }, 1000);
+      }, 5000);
     });
   }
-
-  showBoard(player1.ownBoard);
-  showBoard(player1.ownBoard);
-
 })();
