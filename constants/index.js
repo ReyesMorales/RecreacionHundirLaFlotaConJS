109f28054ae936afdc5bdf5e8028d0c1d62b0223
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-06-23 10:50:13 +0100

    Add comments

--- constants/index.js
+++ constants/index.js
@@ -1,11 +1,32 @@
-const EMPTY_SPACE = ' ';
-const BOAT_SPACE = '🚢';
-const WATER_SPACE = '🌊';
-const HIT_SPACE = '💥';
-const MAX_SHOOTS = 100;
+/**
+ * Represents an empty space on the board.
+ */
+ const EMPTY_SPACE = ' ';
 
-exports.BOAT_SPACE = BOAT_SPACE;
-exports.WATER_SPACE = WATER_SPACE;
-exports.EMPTY_SPACE = EMPTY_SPACE;
-exports.HIT_SPACE = HIT_SPACE;
-exports.MAX_SHOOTS = MAX_SHOOTS;
+ /**
+  * Represents a space occupied by a boat on the board.
+  */
+ const BOAT_SPACE = '🚢';
+ 
+ /**
+  * Represents a space filled with water on the board.
+  */
+ const WATER_SPACE = '🌊';
+ 
+ /**
+  * Represents a space hit by a shoot on the board.
+  */
+ const HIT_SPACE = '💥';
+ 
+ /**
+  * The maximum number of shoots allowed for a player.
+  */
+ const MAX_SHOOTS = 100;
+ 
+ // Export the constants
+ exports.BOAT_SPACE = BOAT_SPACE;
+ exports.WATER_SPACE = WATER_SPACE;
+ exports.EMPTY_SPACE = EMPTY_SPACE;
+ exports.HIT_SPACE = HIT_SPACE;
+ exports.MAX_SHOOTS = MAX_SHOOTS;
+ 
